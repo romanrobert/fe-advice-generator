4dd7bced77a1e4e28614fe26fbce654188477337
romanrobert <iromanrobert@gmail.com>
2022-12-14 16:47:05 +0200

    commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./App.css";
+import Dice from "../src/assets/images/icon-dice.svg";
+import Divider from "../src/assets/images/pattern-divider-desktop.svg";
+
+export default function App() {
+  const [advice, setAdvice] = useState({
+    id: 0,
+    advice: "Generate your random advice.",
+  });
+
+  function getAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => setAdvice(data.slip));
+  }
+
+  return (
+    <article className="advice-card">
+      <div className="advice-container">
+        <p className="advice-id">{`ADVICE #${advice.id}`}</p>
+        <p className="advice">{`"${advice.advice}"`}</p>
+        <img className="divider" src={Divider} />
+        <span className="dice-container">
+          <img className="dice" src={Dice} onClick={getAdvice} />
+        </span>
+      </div>
+    </article>
+  );
+}
